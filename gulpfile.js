7531d832319c271766c73e6a509531c7b56e7ed6
WiL <wfabian.ardila@gmail.com>
2017-09-28 11:30:29 -0500

    Update deploy task to deploy to localhost

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 const gulp= require('gulp')
     , merge= require('merge-stream')
     , clean= require('gulp-dest-clean')
-    , changed= require('gulp-changed');
+    , changed= require('gulp-changed')
+    , exec= require('child_process').exec;
 
-const dest= 'dist';
+const dest= 'dist'
+    , localhostFolder= '/home/wil/www/regatearapi.test/public_html/'
+    , cmdDeployLocalhost= 'rsync -cr ./' + dest + '/ ' + localhostFolder;
 
-gulp.task('deploy', function() {
+gulp.task('prepare_deploy', function() {
     return merge(gulp.src('vendor/**', { base: '.' }),
                  gulp.src('src/**', { dot: true }))
                 .pipe(clean(dest))
                 .pipe(changed(dest))
                 .pipe(gulp.dest(dest));
+});
+
+gulp.task('deploy', ['prepare_deploy'], function(){
+    exec(cmdDeployLocalhost);
 });
